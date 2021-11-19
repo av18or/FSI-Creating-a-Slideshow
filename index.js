caedc1059801f11f2dfdcd714345f861520992bb
Clayton Gorham <claytongorham@gmail.com>
2021-11-18 20:12:27 -0800

    trying to figure out an on/off button

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ const images = [
     './assets/6.jpg'
 ]
 
-// select bubbles
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+//
+
+//
+
+// select all bubbles
 const bubbles = document.querySelectorAll('.bubble')
 
 
